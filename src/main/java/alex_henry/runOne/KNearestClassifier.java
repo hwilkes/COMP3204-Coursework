52c2566ca87a627a20f81f714186df7c654f6c01
Alexander Lay <acl1g12@soton.ac.uk>
2014-11-28 16:21:52 +0000

    Implemented classify method. Refactored class data structures to enable this.

--- src/main/java/alex_henry/runOne/KNearestClassifier.java
+++ src/main/java/alex_henry/runOne/KNearestClassifier.java
@@ -1,46 +1,91 @@
 package alex_henry.runOne;
 
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.PriorityQueue;
 
-import org.openimaj.data.dataset.ListBackedDataset;
 import org.openimaj.data.dataset.ListDataset;
 import org.openimaj.feature.FloatFV;
+import org.openimaj.feature.FloatFVComparison;
 import org.openimaj.image.FImage;
 
 public class KNearestClassifier {
-	
-	HashMap<String,ListDataset<FloatFV>> classes; 
-	
+
+	HashMap<FloatFV, String> classes; 
+
 	public KNearestClassifier()
 	{
-		classes = new HashMap<String,ListDataset<FloatFV>>();
+		classes = new HashMap<FloatFV,String>();
 	}
-	
+
 	public String classify(FImage image,int kValue)
 	{
 		TinyImage tiny = new TinyImage(image,16,16);
-		
+
 		FloatFV v = tiny.getVector();
+
+		PriorityQueue<FloatFV> q = new PriorityQueue<FloatFV>(1,new VectorComparison(v));
+
+		for(FloatFV vector : classes.keySet())
+		{
+			q.add(vector);
+		}
+
+		HashMap<String, Integer> classCount = new HashMap<String, Integer>();
+		//get the k nearest neighbours
+		for(int i = 0; i < kValue ; i++){
+			FloatFV neighbour = q.poll();
+			String neighbourClass = classes.get(neighbour);
+			if(classCount.containsKey(neighbourClass))
+			{
+				classCount.put(neighbourClass, classCount.get(neighbourClass) + 1); 
+			}
+			else
+			{
+				classCount.put(neighbourClass, new Integer(1));
+			}
+		}
+		//determine class using them
+		String targetClass = null;
+		Integer n = 0;
+		//TODO improve case when multiple classes have the same count
+		for(String s : classCount.keySet())
+		{
+			Integer sNum = classCount.get(s);
+			if(sNum > n)
+			{
+				targetClass = s;
+				n = sNum;
+			}
+		}
 		
-		
-		
-		return "POO";
+		return targetClass;
 	}
-	
+
 	public void addClassValues(ListDataset<TinyImage> group,String setName)
 	{
-		if(classes.containsKey(setName)){
-			ListDataset<FloatFV> existing = classes.get(setName);
-			for(TinyImage i : group){
-				existing.add(i.getVector());
-			}
-		} else {
-			ListDataset<FloatFV> vectors = new ListBackedDataset<FloatFV>();
-			for(TinyImage i : group){
-				vectors.add(i.getVector());
-			}
-			classes.put(setName, vectors);
+		for(TinyImage i : group){
+			classes.put(i.getVector(), setName);
 		}
+
 	}
-	
+
+	class VectorComparison implements Comparator<FloatFV>{
+
+		FloatFV target;
+
+		public VectorComparison(FloatFV target){
+			this.target = target;
+		}
+
+		@Override
+		public int compare(FloatFV arg0, FloatFV arg1) {
+			FloatFVComparison comp = FloatFVComparison.EUCLIDEAN;
+			Double dist0 = comp.compare(arg0, target);
+			Double dist1 = comp.compare(arg1, target);
+			return dist0.compareTo(dist1);
+		}
+
+	}
+
 }
