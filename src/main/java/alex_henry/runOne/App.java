bd5a2c4a5917e9cd378c593eb715a0028756226c
Alexander Lay <acl1g12@soton.ac.uk>
2014-12-10 19:02:26 +0000

    Comments!

--- src/main/java/alex_henry/runOne/App.java
+++ src/main/java/alex_henry/runOne/App.java
@@ -13,45 +13,55 @@ import org.openimaj.image.ImageUtilities;
 
 
 /**
- * OpenIMAJ Hello world!
+ * Run One classifier using K nearest neighbours on TinyImage feature vectors
  *
  */
 public class App {
+	
+	//The number of nearest neighbours to analyze.
+	//Determined using output from KDeterminer class
+	private static final int K = 29;
+	
 	public static void main( String[] args ) {
-
+		
 		KNearestClassifier classifier = new KNearestClassifier();
 
-
-		File folder = new File("/home/hw17g12/Downloads/training/");
+		//for each folder in the training data
+		File folder = new File("./images/training/");
 		for(File subFolder : folder.listFiles())
-		{
+		{	
+			//load its images
 			VFSListDataset<FImage> images;
 			try {
+				
 				images = new VFSListDataset<FImage>(subFolder.getAbsolutePath(), ImageUtilities.FIMAGE_READER);
 			} catch (FileSystemException e) {
 				e.printStackTrace();
 				break;
 			}
+			//build tiny image features for each image
 			ListDataset<TinyImage> tinys = new ListBackedDataset<TinyImage>();
 			for(FImage f : images)
 			{
 				tinys.add(new TinyImage(f,16,16));
 			}
+			//add them to the classifiers data
 			classifier.addClassValues(tinys, subFolder.getName());
 
 		}
+		
+		//load a test image from the tall building class
 		FImage testImage;
 		try
 		{
-			testImage = ImageUtilities.readF(new File("/home/hw17g12/Downloads/training/TallBuilding/15.jpg"));
-
+			testImage = ImageUtilities.readF(new File("./images/training/TallBuilding/15.jpg"));
 		}
 		catch (IOException e) {
 			e.printStackTrace();
 			return;
 		}
-
-		String imageClass = classifier.classify(testImage, 9);
+		//show the image in a window
+		String imageClass = classifier.classify(testImage, K);
 		DisplayUtilities.display(testImage, imageClass);
 		
 	}
