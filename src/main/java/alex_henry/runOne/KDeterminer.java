5bdffe5b3b0cb2cbeee566cc45e3ebe3b9a48f1d
Alexander Lay <acl1g12@soton.ac.uk>
2014-12-11 14:23:24 +0000

    Improved data loading.

--- src/main/java/alex_henry/runOne/KDeterminer.java
+++ src/main/java/alex_henry/runOne/KDeterminer.java
@@ -3,6 +3,7 @@ package alex_henry.runOne;
 import alex_henry.common.ImageLoader;
 import alex_henry.common.KFoldCrossValidation;
 import alex_henry.common.KFoldCrossValidation.KFoldResult;
+import alex_henry.common.TrainingData;
 
 public class KDeterminer {
 
@@ -10,10 +11,12 @@ public class KDeterminer {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		
+		TrainingData data = ImageLoader.loadTrainingImages();
+		
 		System.out.println("K,Max Error,Mean Error,Lowest Error");
 		for(int k = 1; k < 100; k++){
-			KFoldResult result = KFoldCrossValidation.getAccuracy(10, ImageLoader.loadTrainingImages(), new RunOne(k));
+			KFoldResult result = KFoldCrossValidation.getAccuracy(10, data, new RunOne(k));
 			System.out.println(k + "," + result.highestError + "," + result.meanError + "," + result.lowestError);
 		}	
 	}
